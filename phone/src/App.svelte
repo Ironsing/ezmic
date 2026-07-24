ddead77a88c683d3d06b6fc0a37608dc8b314929
Ironsing <ironsing@proton.me>
2026-07-24 12:31:02 +0530

    add svelte

---
+++ phone/src/App.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    let count = $state(0);
+
+    function increment() {
+        count += 1;
+    }
+</script>
+
+<main>
+    <h1>Hello from Svelte!</h1>
+
+    <p>Clicks: {count}</p>
+    <button onclick={increment}> Click me </button>
+</main>
+
+<style>
+    main {
+        font-family: sans-serif;
+        text-align: center;
+        padding: 2rem;
+    }
+
+    button {
+        padding: 0.5rem 1rem;
+        font-size: 1rem;
+        cursor: pointer;
+    }
+</style>
---
+++ phone/svelte.config.js
@@ -0,0 +1,6 @@
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+
+export default {
+  // Enables preprocessing for features like TypeScript or PostCSS
+  preprocess: vitePreprocess(),
+}
